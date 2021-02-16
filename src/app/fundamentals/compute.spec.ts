aa369554ce69569504b3197d7323aff42ce90836
MUBHASHIRAWIN10\mubhashira <sayedmubashira@mindfiresolutions.com>
2021-02-16 12:46:45 +0530

    images updated

--- src/app/fundamentals/compute.spec.ts
+++
@@ -1,8 +0,0 @@
-import {compute} from './compute';
-
-describe('compute',function(){
-    it('should return 0 if input is negative',function(){
-        const result=compute(-1);
-        expect(result).toBe(0);
-    })
-})
